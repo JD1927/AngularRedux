565cd6715d53dc4b793a0b86a1ec0efe86e1efaf
JD1927 <juanaguirre237288@correo.itm.edu.co>
2019-04-14 12:01:03 -0500

    Agrega configuración de los Auth Actions de NgRX

--- src/app/auth/store/actions/auth.actions.ts
+++ src/app/auth/store/actions/auth.actions.ts
@@ -1,55 +1,46 @@
 import { Action } from '@ngrx/store';
-import { IUser } from 'src/app/shared/interfaces/user.model';
+import { User } from '../../models/user';
 
-export enum AuthActionsType  {
-  LoggedUser = '[Auth] LOGGED_USER',
-  LoginUser = '[Auth] LOGIN_USER',
-  LoginUserError = '[Auth] LOGIN_USER_ERROR',
-  LoggedIn = '[Auth] LOGGED_IN',
-  LogOut = '[Auth] LOG_OUT',
+// Auth ACTIONS
+export const GET_USER = '[Auth] GET USER';
+export const AUTHENTICATED = '[Auth] AUTHENTICATED';
+export const NOT_AUTHENTICATED = '[Auth] NOT AUTHENTICATED';
+export const GOOGLE_LOGIN = '[Auth] GOOGLE LOGIN ATTEMPT';
+export const LOG_OUT = '[Auth] LOG OUT';
+export const AUTH_ERROR = '[Auth] AUTH ERROR';
+
+export class GetUser implements Action {
+  readonly type = GET_USER;
+  constructor(public payload?: any) { }
+}
+export class Authenticated implements Action {
+  readonly type = AUTHENTICATED;
+  constructor(public payload?: any) { }
 }
 
+export class NotAuthenticated implements Action {
+  readonly type = NOT_AUTHENTICATED;
+  constructor(public payload?: any) { }
+}
 
-export const loggedUser = '[Auth] LOGGED_USER';
-export const loginUser = '[Auth] LOGIN_USER';
-export const loginUserError = '[Auth] LOGIN_USER_ERROR';
-export const loggedIn = '[Auth] LOGGED_IN';
-export const logOut = '[Auth] LOG_OUT';
-
-/**
- * Every action is comprised of at least a type and an optional
- * payload. Expressing actions as classes enables powerful
- * type checking in reducer functions.
- */
-export class LoggedIn implements Action {
-  readonly type = loggedIn;
-
-  constructor(public payload: { isLogin: boolean }) { }
+export class GoogleLogin implements Action {
+  readonly type = GOOGLE_LOGIN;
+  constructor(public payload?: any) { }
 }
 
 export class LogOut implements Action {
-  readonly type = logOut;
-
-  constructor(public payload: { isLogout: boolean }) { }
-}
-export class LoggedUser implements Action {
-  readonly type = loggedUser;
-
-  constructor(public payload: { isLoading: boolean, error: boolean, user: IUser }) { }
+  readonly type = LOG_OUT;
+  constructor(public payload?: any) { }
 }
 
-export class LoginUserError implements Action {
-  readonly type = loginUserError;
-
-  constructor(public payload: { error: string }) { }
+export class AuthError implements Action {
+  readonly type = AUTH_ERROR;
+  constructor(public payload?: any) { }
 }
-export class LoginUser implements Action {
-  readonly type = loginUser;
 
-  constructor(public payload: { user: IUser }) { }
-}
-/**
- * Export a type alias of all actions in this action group
- * so that reducers can easily compose action types
- */
-export type actions = LoggedIn | LogOut | LoggedUser | LoginUserError | LoginUser;
+export type auth = GetUser
+| Authenticated
+| NotAuthenticated
+| GoogleLogin
+| LogOut
+| AuthError;
